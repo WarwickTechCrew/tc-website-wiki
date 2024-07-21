5eb875cb669b70dd999648e9400d37ca283e0a59
Josh Heng <josh@joshheng.co.uk>
2024-07-21 21:21:15 +0100

    :recycle: Organise wiki and add external links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,9 +44,8 @@ const config: Config = {
         docs: {
           path: './wiki',
           routeBasePath: 'wiki',
-          sidebarPath: './sidebars.ts',
-          editUrl:
-            'https://github.com/WarwickTechCrew/website/tree/main/',
+          sidebarPath: './sidebar.ts',
+          editUrl: 'https://github.com/WarwickTechCrew/website/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -67,7 +66,7 @@ const config: Config = {
       items: [
         {
           type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
+          sidebarId: 'wiki',
           position: 'left',
           label: 'Wiki',
         },

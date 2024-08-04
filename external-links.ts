401f830a9631c005a2d918d3436eee9e46d89664
Josh Heng <josh@joshheng.co.uk>
2024-08-04 20:38:19 +0100

    :fire: Remove external links

--- external-links.ts
+++ external-links.ts
@@ -7,11 +7,6 @@ export type ExternalLink = {
 const externalLinks: ExternalLink[] = [
   {
     type: 'link',
-    href: 'https://drive.google.com/drive/folders/1aAAnOl6FXuQ0Mxz1T8VKW7PxtIeVrSSp?usp=sharing',
-    label: 'Public Resources Drive',
-  },
-  {
-    type: 'link',
     href: 'https://drive.google.com/drive/folders/1qcEIQINiboPD26nMSB-obGPqv5tbVeuY?usp=sharing',
     label: 'Exec Meeting Minutes',
   },

75de9b225c602816b433d52f6296ed987f4ef4e9
Kishan Sharma <contact@kishan.app>
2025-07-21 00:22:00 +0100

    refactor to put text files outside component

---
+++ src/components/flags/flags.ts
@@ -0,0 +1,93 @@
+const questions = [
+  {
+    question: 'Are you/have you been/are you open to being a Tech Crew (or associate) member?',
+    colour: 'green',
+    flag: 'Is/has been/is open to being a Tech Crew (or associate) member',
+  },
+  { question: 'Do you contribute to the TC Wiki?', colour: 'green', flag: 'Contributes to the TC Wiki' },
+  {
+    question: 'Do you want to trade hoodies (or other clothes)?',
+    colour: 'green',
+    flag: 'Wants to trade hoodies (or other clothes)',
+  },
+  {
+    question: 'Do you offer to wash up if someone else cooks?',
+    colour: 'green',
+    flag: 'Offers to wash up if you cook',
+  },
+  { question: "Do you get on with your partner's siblings?", colour: 'green', flag: 'Gets on with your siblings' },
+  { question: 'Would you come to parkrun and pancakes?', colour: 'green', flag: 'Comes to parkrun and pancakes' },
+  {
+    question: 'Do you listen to songs that are recommended to you?',
+    colour: 'green',
+    flag: 'Listens to song you recommended',
+  },
+  {
+    question: 'Do you know the difference between DMX and XLR cable?',
+    colour: 'green',
+    flag: 'Knows the difference between DMX and XLR cable',
+  },
+  {
+    question: "Does Trin's mum get a 'good vibe' from you?",
+    colour: 'green',
+    flag: 'Trin\'s mum gets a "good vibe" from them',
+  },
+  { question: 'Do you shower frequently?', colour: 'green', flag: 'Showers frequently' },
+  {
+    question: 'Do you wait for everyone to have food before starting (within reason)?',
+    colour: 'green',
+    flag: 'Waits for everyone to have food before starting (within reason)',
+  },
+  {
+    question: "Do you offer to paint someone's other hand when they're painting their nails?",
+    colour: 'green',
+    flag: "Offers to paint your other hand when you're painting your nails",
+  },
+  {
+    question: 'Have you watched MCU up til phase 4 (or are you open to watch them)?',
+    colour: 'green',
+    flag: 'Has watched MCU up til phase 4 (or is open to watch them)',
+  },
+  {
+    question: "Do you say 'happy tomorrow' at midnight not 'happy today'?",
+    colour: 'green',
+    flag: 'Says "happy tomorrow" at midnight not "happy today"',
+  },
+  { question: 'Do you join discord vc?', colour: 'green', flag: 'Joins discord vc' },
+  { question: 'Do you have soft toys?', colour: 'green', flag: 'Has soft toys' },
+  {
+    question: 'Do you like Swedish meatballs (if not vegetarian)?',
+    colour: 'green',
+    flag: 'Likes Swedish meatballs (if not vegetarian)',
+  },
+  { question: 'Do you like spontaneous IKEA trips?', colour: 'green', flag: 'Likes spontaneous IKEA trips' },
+  { question: 'Do you appreciate kids animated movies?', colour: 'green', flag: 'Appreciates kids animated movies' },
+  {
+    question: "Would you refuse to borrow someone's hoodie because of gender stereotypes?",
+    colour: 'red',
+    flag: "Won't borrow your hoodie because of gender stereotypes",
+  },
+  { question: "Do your partner's siblings hate you?", colour: 'red', flag: 'Siblings hate them' },
+  { question: 'Do you hate parkrun?', colour: 'red', flag: 'Hates parkrun' },
+  {
+    question: "Does Trin's mum get a 'bad vibe' from you?",
+    colour: 'red',
+    flag: 'Trin\'s mum gets a "bad vibe" from them',
+  },
+  {
+    question: 'Do you grow mould in your room (not for the sake of science)?',
+    colour: 'red',
+    flag: 'Grows mould in their room (not for the sake of science)',
+  },
+  { question: 'Do you dislike cats?', colour: 'red', flag: "Doesn't like cats" },
+  { question: "Do you avoid talking to Trin's mum?", colour: 'red', flag: "Doesn't talk to Trin's mum" },
+  { question: 'Do you refuse to do ski pole ears?', colour: 'red', flag: "Doesn't do ski pole ears" },
+  {
+    question: 'Do you not wave back at people who wave at you?',
+    colour: 'red',
+    flag: "Doesn't wave back at people who wave at them",
+  },
+  { question: "Do you not react to Josh's sad face?", colour: 'red', flag: "Doesn't react to Josh's sad face" },
+];
+
+export default questions;
---
+++ src/components/flags/resultsMessage.ts
@@ -0,0 +1,82 @@
+const getResultMessage = (greenFlags, redFlags, maxScore) => {
+  // max green flags
+  const pick = (arr: string[]) => arr[Math.floor(Math.random() * arr.length)];
+  if (greenFlags === maxScore && redFlags === 0) {
+    return pick([
+      "Perfect! You're basically the ideal person :D",
+      '100% squeaky clean and all green.',
+      'you are green like the grinch. i feel like a who (from whoville)',
+    ]);
+  }
+  if (greenFlags === maxScore && redFlags === 1) {
+    return '💚 oh my- you have,, is that all? all the green flags??? you have all the green flags. but you manage to stay cool, and edgy, with your single red flag.';
+  }
+  if (greenFlags === maxScore && redFlags === maxScore) {
+    return pick([
+      `how did you get all the flags??? you didn't just spam the yes button did you ????`,
+      'please stop spamming the yes button',
+      'you have all the flags.. but you also have all the red flags... how- how did you do that?',
+      'it is not possible for ONE person to have ALL the flags',
+      'please donate some of your flags to charity',
+    ]);
+  }
+  if (greenFlags === maxScore && redFlags > 0) {
+    return pick([
+      `Amazing green flags, you have them ALL, but hmmm ${redFlags} red flag${redFlags > 1 ? 's' : ''} to work on... can I tell you a secret? even the best people have flaws. It's what you do with them that counts.`,
+      `Amazing green flags, you have them ALL, but hmmm ${redFlags} red flag${redFlags > 1 ? 's' : ''} to work on...`,
+    ]);
+  }
+  // some green flags but not all
+  if (redFlags === 0 && greenFlags > 0) {
+    return pick([`✨ Clean slate with no red flags! ${'yip '.repeat(greenFlags).trim()}peeeee`, 'touch grass?']);
+  }
+  if (greenFlags >= maxScore * 0.8 && redFlags <= 1) {
+    return pick([
+      'Yippeee! Lots of green flags with minimal red ones! I think i can fix you',
+      `this is a secret message for people with ${greenFlags} green flags and EXACTLY ${redFlags} red flag${redFlags > 1 ? 's' : ''}: makka pakka`,
+    ]);
+  }
+  if (greenFlags > redFlags) {
+    return pick([
+      `${greenFlags} green flags outweigh ${redFlags} red flag${redFlags > 1 ? 's' : ''}! i mean you could do better. you can always do better. but you are doing well.`,
+      `you are ${(100 * greenFlags) / greenFlags + redFlags}% green flag. thats pretty good. i hope that percentage is correct because i did it in my head. actually, looking at it, im quite sure that that percentage is wrong.`,
+      `you are ${(100 * greenFlags) / (greenFlags + redFlags)}% green flag. thats pretty good. i hope that percentage is correct because i did it in my head.`,
+      `if we were in a boat and your green flags were air and your red flags were water then we would survive for a while. but not long. because of the water.`,
+      `you have more green flags than red flags. which means your green flags are winning. which means you are winning.`,
+      `you have ${greenFlags} green flags. which is good. because green flags are good. and you have more green flags than red flags. which is also good. which means you are good.`,
+    ]);
+  }
+  if (greenFlags === redFlags && greenFlags > 0) {
+    return pick([
+      "woah... Perfectly balanced - equal green and red flags. i- i don't know what to say. this has never happened before. you fall deeper in cosmic harmony with the universe every passing day. i- want to be you.",
+      'if green flags where XLR cables and red flags where DMX cables then i would have the same number of XLR cables as i would DMX cables.',
+      'imagine two things that are the same. are you thinking of the same two things? because i am. and they are the same. like you and your red flags and green flags.',
+    ]);
+  }
+  if (redFlags > greenFlags && greenFlags > 0) {
+    return pick([
+      `${redFlags} red flags vs ${greenFlags} green flag${greenFlags > 1 ? 's' : ''}. I'm not sure if we can be friends anymore... well unless you work on your red flags. and maybe pick up a few more green ones. or at least paint your red flags green. or if i became colour blind. or if the dictionary swapped the words red and green. then we could be friends`,
+      `if we were swimming and you put ${redFlags} stones in my pockets i would die`,
+      "aww thats okay buddy. you have some green flags, and that's what matters.",
+      `imagine i am reading your tarot cards. i see the ${redFlags} of wands and the ${greenFlags} of pentacles. the ${redFlags} of wands represents your red flags, and the ${greenFlags} of pentacles represents your green flags. i don't know what that means but it sounds bad.`,
+    ]);
+  }
+  // no green flags
+  if (redFlags > 0 && greenFlags === 0) {
+    return pick([
+      `🚩 Yikes... ${redFlags} red flag${redFlags > 1 ? 's' : ''} and no green ones... how will you redeem yourself (commit to tc wiki)`,
+      'loser alert',
+    ]);
+  }
+  if (redFlags === 0 && greenFlags === 0) {
+    return pick([
+      "did you just spam the no button :( that's okay i guess.. ",
+      'please stop spamming my buttons :(',
+      'maybe someone will donate you some flags?',
+      'you poor flagless person..',
+    ]);
+  }
+  return 'idk what happened here';
+};
+
+export default getResultMessage;
